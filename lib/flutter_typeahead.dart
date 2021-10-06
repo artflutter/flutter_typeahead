7a5263c047908d4e84e8a5f7e0ba3b1585727cad
vasilich <vasilich@grafin.kiev.ua>
2021-10-06 15:28:58 +0300

    test

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -1,9 +1,9 @@
 library flutter_typeahead;
 
 import 'dart:async';
-
-export 'src/typedef.dart';
-export 'src/flutter_typeahead.dart';
-export 'src/cupertino_flutter_typeahead.dart';
+//
+// export 'src/typedef.dart';
+// export 'src/flutter_typeahead.dart';
+// export 'src/cupertino_flutter_typeahead.dart';
 
 typedef FutureOr<Iterable<T>> SuggestionsCallback1<T>(String pattern);
